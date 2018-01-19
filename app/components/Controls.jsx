bcd9ac99449bb380c345700348194d77e80656a7
Nazar <kryvyynazariy@gmail.com>
2018-01-19 15:17:31 +0200

    Add static control buttons

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -1,26 +1,26 @@
 var React = require('react');
 
-var Controls = React.creatClass({
+var Controls = React.createClass({
     propTypes: {
-        countdownStatu: React.PtopType.string.isRequired
+        countdownStatus: React.PropTypes.string.isRequired
     },
-    render: function(){
-        var {countdownStatu} = this.props;
-        var renderStartStopButton = () =>{
-            if(countdownStatu ==='started'){
-                return <button className="button secondary">Pause</button>
-            }else if(countdownStatu ==='paused'){
+    render: function () {
+        var {countdownStatus} = this.props;
+        var renderStartStopButton = () => {
+            if (countdownStatus === 'started') {
+                return <button className = "button secondary">Pause</button>
+            } else if (countdownStatus === 'paused') {
                 return <button className="button primary">Start</button>
             }
         }
-        return(
+
+        return (
             <div className='controls'>
                 {renderStartStopButton()}
                 <button className="button alert hollow">Clear</button>
             </div>            
         );
     }
-
 });
 
 module.exports = Controls;
--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -2,8 +2,9 @@ var React = require('react');
 
 var Timer = React.createClass({
   render: function () {
-    return <p>Timer.jsx</p>;
+    return 
   }
 });
+
 
 module.exports = Timer;
